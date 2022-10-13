bdfbe1ad80d58272ddd06333ef4b3be38286c1c6
Andrei <a.parchukov@yandex.ru>
2022-10-13 16:42:44 +0300

    Initial Commit

---
+++ RecognitionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecognitionApp
+//
+//  Created by Андрей Парчуков on 13.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecognitionApp/RecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognitionAppApp.swift
+//  RecognitionApp
+//
+//  Created by Андрей Парчуков on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
